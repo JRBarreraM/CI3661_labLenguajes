ff516ccf6501a89c1d350e6631af1608745166ad
Carlos Rivero <carlosrivero96@gmail.com>
2020-01-30 20:45:21 -0400

    Added Show for data types and first functions

--- JackLambda/Cartas.hs
+++ JackLambda/Cartas.hs
@@ -12,28 +12,61 @@
 --Surrender: Después de recibir la mano inicial, el jugador puede decidir
 --no jugar la ronda y rendirse, cediendo la mitad de su apuesta.
 
-data Palo = Treboles | Diamantes | Picas | Corazones
-data Rango = N Int | Jack | Queen | King | Ace
+data Palo = Treboles | Diamantes | Picas | Corazones deriving (Eq, Enum)
+data Rango = N Int | Jack | Queen | King | Ace deriving Eq
 
 data Carta = Carta {
     rango :: Rango,
     palo :: Palo
-}
+} deriving Eq
 
 data Jugador = Dealer | Player
 
-newtype Mano = Mano [Card]
+newtype Mano = Mano [Carta] deriving Show
+
+instance Show Palo where
+    show Treboles = "♣"
+    show Diamantes = "♢"
+    show Picas = "♠"
+    show Corazones = "♡"
+
+instance Show Rango where
+    show (N i) = show i
+    show Jack = "J"
+    show Queen = "Q"
+    show King = "K"
+    show Ace = "A"
+        
+instance Show Carta where
+    show (Carta r p) = show p ++ show r
 
 --Funciones de construcción:
 vacia :: Mano
+vacia = Mano []
 
 baraja :: Mano
+baraja = Mano [ (Carta x y) | x <- [N 2, N 3, N 4, N 5, N 6, N 7, N 8, N 9, N 10] ++ [Jack, Queen, King, Ace], y <- [Treboles .. Corazones] ]
 
 --Funciones de acceso:
-cantidad Cartas :: Mano ->Int
+cantidadCartas :: Mano ->Int
+cantidadCartas (Mano x) = length x
 
 valor :: Mano ->Int
+valor (Mano []) = 0
+valor (Mano (c:cs)) =
+    if valorCarta(c) + valor (Mano cs) > 21 && valorCarta(c) == 11
+        then 1 + valor (Mano cs)
+        else valorCarta(c) + valor (Mano cs)
 
+valorCarta :: Carta -> Int
+valorCarta (Carta r p) = case r of
+    N i -> i
+    Jack -> 10
+    Queen -> 10
+    King -> 10
+    Ace -> 11
+
+{-
 busted :: Mano ->Bool
 
 blackjack :: Mano ->Bool
@@ -63,3 +96,4 @@ reconstruir :: Mazo ->Mano ->Mazo
 robar :: Mazo ->Mano ->Eleccion ->Maybe (Mazo,Mano)
 
 juegaLambda :: Mazo ->Mano ->Maybe Mano
+-}
