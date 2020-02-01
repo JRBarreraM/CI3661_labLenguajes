a581a24b318099d272920a12fbc5d81fa8e6dc54
Carlos Rivero <carlosrivero96@gmail.com>
2020-02-01 00:10:12 -0400

    Added separar, show Mano and fixed valor

--- JackLambda/Cartas.hs
+++ JackLambda/Cartas.hs
@@ -12,6 +12,8 @@
 --Surrender: Después de recibir la mano inicial, el jugador puede decidir
 --no jugar la ronda y rendirse, cediendo la mitad de su apuesta.
 
+import System.Random
+
 data Palo = Treboles | Diamantes | Picas | Corazones deriving (Eq, Enum)
 data Rango = N Int | Jack | Queen | King | Ace deriving Eq
 
@@ -22,7 +24,7 @@ data Carta = Carta {
 
 data Jugador = Dealer | Player deriving Show
 
-newtype Mano = Mano [Carta] deriving Show
+newtype Mano = Mano [Carta]
 
 instance Show Palo where
     show Treboles = "♣"
@@ -36,9 +38,13 @@ instance Show Rango where
     show Queen = "Q"
     show King = "K"
     show Ace = "A"
-        
+
 instance Show Carta where
     show (Carta r p) = show p ++ show r
+
+instance Show Mano where
+    show (Mano [c]) = show c
+    show (Mano (c:cs)) = (show c) ++ (show (Mano cs))
 
 --Funciones de construcción:
 vacia :: Mano
@@ -51,15 +57,14 @@ baraja = Mano [ (Carta x y) | x <- [N 2, N 3, N 4, N 5, N 6, N 7, N 8, N 9, N 10
 cantidadCartas :: Mano ->Int
 cantidadCartas (Mano x) = length x
 
-valor :: Mano ->Int
-valor (Mano []) = 0
-valor (Mano (c:cs)) = valorCarta(c) + valor (Mano cs)
 
-{-
-    if valorCarta(c) + valor (Mano cs) > 21 && valorCarta(c) == 11
-        then 1 + valor (Mano cs)
-        else valorCarta(c) + valor (Mano cs)
--}
+valor :: Mano ->Int
+valor (Mano x) | sum (map (valorCarta) x) > 21 = (sum (map (valorCarta) x)) - (10 * (numTimesFound 11 (map (valorCarta) x)))
+               | otherwise = sum (map (valorCarta) x)
+
+numTimesFound :: Eq a => a -> [a] -> Int
+numTimesFound x xs = (length . filter (==x)) xs
+
 
 valorCarta :: Carta -> Int
 valorCarta (Carta r p) = case r of
@@ -76,18 +81,29 @@ busted x = valor (x) > 21
 blackjack :: Mano ->Bool
 blackjack (Mano x) = valor(Mano x) == 21 && length x == 2
 
-ganador :: Mano ->Mano ->Jugador --Preguntar a Jean por empate
-ganador x y | busted(x) || blackjack(y) = Player
-            | busted(y) || blackjack(x) = Dealer
+ganador :: Mano ->Mano ->Jugador
+ganador x y | busted(y) || blackjack(x) = Dealer
+            | busted(x) || blackjack(y) = Player
             | valor(x) > valor(y) || valor(x) == valor(y) = Dealer
             | valor(x) < valor(y) = Player
-{-
+
 separar :: Mano ->(Mano, Carta, Mano)
-separar x | odd (length x) = 
-          | even (length x) = 
--}
+separar (Mano x) = (Mano (take ((length x)`div`2) x), x!!((length x)`div`2), Mano (drop ((length x)`div`2+1) x))
+
 --Funciones de modificación:
 --barajar :: StdGen ->Mano ->Mano
+--barajar g0 (Mano x) = Mano (shuffle x)
+
+{-
+shuffle :: [a] -> [a]
+shuffle x = if length x < 2 then return x else do
+    i <- System.Random.randomRIO (0, length(x)-1)
+    r <- shuffle (take i x ++ drop (i+1) x)
+    return (x!!i : r)
+-}
+
+--  rollDice :: IO Int
+--  rollDice = getStdRandom (randomR (1,6))
 
 inicialLambda :: Mano ->(Mano, Mano)
 inicialLambda (Mano (x:y:xs)) = (Mano [x,y], Mano xs)
